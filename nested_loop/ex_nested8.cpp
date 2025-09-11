c8f00d14aa9639465c1329a1c6be784c12b26d40
Ronit Kapar <work.ronitkapar@gmail.com>
2025-09-11 11:40:47 +0545

    added a new solution

---
+++ nested_loop/ex_nested8.cpp
@@ -0,0 +1,19 @@
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 4;
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j < i; j++){
+            cout<<i - j<<" "; 
+        }
+        cout<<endl;   
+    }
+    return 0;
+}
