d503a9bb7eb39a215d830d82c2b191f38656ff8c
Ronit Kapar <work.ronitkapar@gmail.com>
2025-09-13 19:58:56 +0545

    added more files

---
+++ nested_loop/ex_nested13.cpp
@@ -0,0 +1,29 @@
+// AAAA
+//  BBB
+//   CC
+//    D
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 4;
+    char ch = 'A';
+
+    for(int i = 0; i < n; i++){
+        
+        // spaces
+        for(int j = 0; j < i; j++){
+            cout<<" ";
+        }
+
+        // alphabets
+        for(int j = 0; j < n - i; j++){
+            cout<<ch;
+        }
+        ch++;
+        cout<<endl;
+    }
+    return 0;
+}
