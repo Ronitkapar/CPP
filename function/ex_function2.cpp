7f0e54634ffee6d65139566f8a0a6ebe63ef34b8
Ronit Kapar <work.ronitkapar@gmail.com>
2025-09-14 17:36:39 +0545

    wrote a program using function to calculate factorial of n

---
+++ function/ex_function2.cpp
@@ -0,0 +1,17 @@
+// calculate n factorial
+
+#include <iostream>
+using namespace std;
+
+int factN(int n){
+    int fact = 1;
+    for(int i = 1; i <= n; i++){
+        fact = fact*i;
+    } 
+    return fact;
+}   
+
+int main(){
+    cout<<factN(5)<<endl;
+    return 0;
+}
